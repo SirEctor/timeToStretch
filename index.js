b1fe53c24353f09867a2d9b4fd8ae468e8b95f92
SirEctor <ebenezer.teshome@duke.edu>
2024-08-01 10:38:03 -0400

    notifications!

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 console.log("INDEX JS ACTIVATED!")
+
+function sendNotification() {
+    browser.notifications.create({
+      "type": "basic",
+      "iconUrl": browser.extension.getURL("icons/icon2.png"),
+      "title": "TimeToStretch - Hourly Check!",
+      "message": "This is your hourly check in to:\nstretch 🤸\nhydrate 💧\nexercise 🏋️"
+    });
+}
+
+sendNotification();
+//now we need to send notifications at a regular interval
+
+function hourlyNotification(){
+    setInterval(sendNotification, 60 * 60 * 1000)
+}
+
+browser.runtime.onInstalled.addListener(() => {
+    hourlyNotification();
+});
